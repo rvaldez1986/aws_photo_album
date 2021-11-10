eea39332688d4f97977b220ddee9f5ac5c9459eb
Roberto <roberto.valdez.ponce@gmail.com>
2021-11-10 18:24:47 -0500

    modified lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import json
 
 def lambda_hadler(event, context):
     print('Hello from Lambda')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    } 
